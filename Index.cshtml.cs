2fa664f5f72cf9449d36c914db3c3d393c64fc41
taylor18915 <54372304+taylor18915@users.noreply.github.com>
2019-10-02 14:57:00 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace movies.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] movienames { get; set; }
+        public void OnGet()
+        {
+            movienames = new string[] { " madea1", "madea2", " madea3 " };
+        }
+       
+    }
+}
